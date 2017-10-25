161c7b17d2ccdf4adc39eb45abce57791c3e2573
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-25 01:27:02 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  n = 0
-  array = Array.new
-  until n == 4
-    array << n
-    n++
-  end
+  array = Array.new(4)
 end
 
 def add_element_to_end_of_array(array, element)
